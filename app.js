5551168394d17ce6e24f1582c2fb746b8e805212
BabaBoi1735 <andrewvanbeijnen2003@gmail.com>
2025-08-07 00:02:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const profileBtn = document.getElementById('profile-btn');
 const profileButton = document.getElementById('profile-button');
 
+function jwt_decode(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
 function handleCredentialResponse(response) {
   const user = jwt_decode(response.credential);
   console.log(user);
   profileBtn.style.display = 'block';
   profileButton.textContent = `Profiel: ${user.name}`;
+  window.location.href = 'https://cloudifydb.netlify.app/';
 }
 
 google.accounts.id.initialize({
